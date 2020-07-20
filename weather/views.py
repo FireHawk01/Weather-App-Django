5ed4d5d451a7f5e6b80c025b99576a9b2157335e
FireHawk01 <vivekkumar6483@gmail.com>
2020-07-20 14:08:50 +0530

    Connected database and form

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 # Create your views here.
 
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=6ee90d6ab14055e4c56029b48d3cc7fa'
-    city = 'Kanpur'
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save() 
+    form = CityForm()
+
     cities = City.objects.all()
+
     weather_data= []
     for city in cities:
         r = requests.get(url.format(city)).json()
@@ -18,5 +24,5 @@ def index(request):
                 }
         weather_data.append(city_weather)
     #print (city_weather)  
-    context = {'weather_data' : weather_data}
+    context = {'weather_data' : weather_data, 'form' : form}
     return render(request, 'weather/weather.html', context)
